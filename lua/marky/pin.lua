881003b22d839111c97794b6d50feff6f91d7661
MahmoudESSE <mahmoudessehayli@gmail.com>
2023-12-25 09:49:25 +0100

    fix(pin): add checks for empty path and list

--- lua/marky/pin.lua
+++ lua/marky/pin.lua
@@ -4,9 +4,15 @@ local marks = require("marky.settings")
 local M = {}
 
 M.pin = function()
-    print("MarkyPinning")
-    vim.fn.expand("%")
-    table.insert(marks.marks, vim.fn.expand("%"))
+    -- path to the pinned file
+    ---@type string
+    local file_path = vim.fn.expand("%:t")
+
+    if not marks.marks and not file_path then
+        return
+    end
+
+    table.insert(marks.marks, file_path)
 end
 
 return M
