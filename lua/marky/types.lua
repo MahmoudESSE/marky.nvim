b579448558cc2758aa2c7f0d1f43e54ad7f85e11
MahmoudESSE <mahmoudessehayli@gmail.com>
2023-12-25 09:50:01 +0100

    docs(types): add pin desc and list types

--- lua/marky/types.lua
+++ lua/marky/types.lua
@@ -3,15 +3,26 @@
 ---@class Marky
 ---@field options MarkyOptions
 ---@field setup fun(opts: MarkyOptions?)
----@field initialize fun(opts: MarkyOptions?)
 ---@field marks string[]
 ---@field pin MarkyPin
----@field list fun()
+---@field list MarkyList
 
 ---@alias MkOrder "asc" | "desc"
 
 ---@class MarkyOptions
+-- order of the list items
 ---@field order MkOrder?
+-- position of the window
+---@field position "top" | "bottom" | "left" | "right"
+-- width of the window
+---@field width number?
+-- heigth of the window
+---@field heigth number?
 
 ---@class MarkyPin
+-- pin a file
 ---@field pin fun()
+
+---@class MarkyList
+-- list the marked pins
+---@field list fun()
